3d5b18928a7b6bc1f71c3d129d15b58ff9124e30
Ihor-Chupryna <igchup1985@gmail.com>
2023-03-12 21:14:17 +0200

    structure

--- src/app.ts
+++ src/app.ts
@@ -1,72 +1,22 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 
-import { User } from "./models/User.model";
-import { IUser } from "./types/user.types";
+import { configs } from "./configs/config";
+import { userRouter } from "./routers/user.router";
+import { IError } from "./types/common.types";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get(
-  "/users",
-  async (req: Request, res: Response): Promise<Response<IUser[]>> => {
-    const users = await User.find();
+app.use("/users", userRouter);
 
-    return res.json(users);
-  }
-);
-
-app.get(
-  "/users/:userId",
-  async (req: Request, res: Response): Promise<Response<IUser>> => {
-    const { userId } = req.params;
-    const user = await User.findById(userId);
-
-    return res.json(user);
-  }
-);
-
-app.post(
-  "/users",
-  async (req: Request, res: Response): Promise<Response<IUser>> => {
-    try {
-      const body = req.body;
-      const user = await User.create({ ...body });
-
-      return res.status(201).json({
-        message: "User created",
-        data: user,
-      });
-    } catch (e) {
-      // throw new Error(e.message);
-      res.json({ message: e.message });
-    }
-  }
-);
-
-app.put(
-  "/users/:userId",
-  async (req: Request, res: Response): Promise<Response<IUser>> => {
-    const { userId } = req.params;
-    const user = req.body;
-    const updatedUser = await User.updateOne({ _id: userId }, user);
-
-    return res.status(200).json({
-      message: "User updated",
-      data: updatedUser,
-    });
-  }
-);
-
-app.delete("/users/:userId", async (req: Request, res: Response) => {
-  const { userId } = req.params;
-
-  await User.deleteOne({ _id: userId });
-
-  res.status(200).json({
-    message: "User deleted",
+app.use((err: IError, req: Request, res: Response, next: NextFunction) => {
+  const status = err.status;
+  res.status(status).json({
+    message: err.message,
+    status,
   });
 });
 
@@ -74,9 +24,7 @@ app.get("/welcome", (req: Request, res: Response) => {
   res.send("welcome");
 });
 
-const PORT = 5100;
-
-app.listen(PORT, () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/myDataBase");
-  console.log(`Server has started on PORT ${PORT}`);
+app.listen(configs.PORT, () => {
+  mongoose.connect(configs.DB_URL);
+  console.log(`Server has started on PORT ${configs.PORT}`);
 });
